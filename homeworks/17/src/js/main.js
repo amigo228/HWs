0b7f8fcff5ca649f8ed8b8f5d3d347ddfb8334f5
Rostyslav Rudyi <amiguys9@email.com>
2022-05-13 18:12:39 +0200

    js fix

--- homeworks/17/src/js/main.js
+++ homeworks/17/src/js/main.js
@@ -2,9 +2,5 @@ let navOpener = document.querySelector('.js-nav-opener');
 let header = document.querySelector('.header');
 
 navOpener.addEventListener('click', () => {
-    if (header.classList.contains('active')) {
-        header.classList.remove('active');
-    } else {
-        header.classList.add('active');
-    }
+    header.classList.toggle('active');
 });
